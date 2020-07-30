78141415af6fd05b37f45173821826c981d0cb02
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-07-29 22:42:10 -0700

    Firebase database and storage setup done
    Firebase database and storage setup done. UploadForm done.

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAURymT3b0EWHeDurAOsRgCKlgQpKJOvs4",
+    authDomain: "firegram-54e58.firebaseapp.com",
+    databaseURL: "https://firegram-54e58.firebaseio.com",
+    projectId: "firegram-54e58",
+    storageBucket: "firegram-54e58.appspot.com",
+    messagingSenderId: "297166282912",
+    appId: "1:297166282912:web:0ad92ae9e89ea4cf2ba9c5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export { projectStorage, projectFirestore };
