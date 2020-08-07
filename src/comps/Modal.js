b43771fc1980e60f6fab41588f95aadbf220da87
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-08-07 09:05:27 -0700

    Image animation added
    Image animation added

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Modal = ({ selectedImg, setSelectedImg }) => {
 
@@ -10,8 +11,13 @@ const Modal = ({ selectedImg, setSelectedImg }) => {
     }
 
     return (
-        <div className="backdrop" onClick={handleClick}>
-            <img src={selectedImg} alt="enlarged pic" />
+        <div className="backdrop" onClick={handleClick}
+        initial={{opacity: 0}}
+        animate = {{ opacity: 1}}>
+            <motion.img src={selectedImg} alt="enlarged pic" 
+            initial={{y: "-100vh"}}
+            animate={{y: 0}}
+            />
         </div>
     )
 }
